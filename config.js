c729e85aa025a3d84065dc3fdc11de275da892dd
kxxoling <kxxoling@gmail.com>
2016-04-01 20:00:16 +0800

    实现登录函数和视图

--- config.js
+++ config.js
@@ -3,3 +3,4 @@
 exports.TIMELINE_URL = 'https://python-china.org/api/topics/timeline?show=all';
 exports.HOME_URL = 'https://python-china.org/api/topics/timeline';
 exports.DEFAULT_AVATAR = require('./assets/python.png')
+exports.LOGIN_URL = 'https://python-china.org/session'
