cdc697d5f6a44fda42c5bfa550a5fbb51184b9af
kxxoling <kxxoling@gmail.com>
2016-04-02 15:30:29 +0800

    Update config

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
-'use strict';
+const BASE_URL = 'https://python-china.org/api'
 
-exports.TIMELINE_URL = 'https://python-china.org/api/topics/timeline?show=all';
-exports.HOME_URL = 'https://python-china.org/api/topics/timeline';
-exports.DEFAULT_AVATAR = require('./assets/python.png')
-exports.LOGIN_URL = 'https://python-china.org/session'
+export const DEFAULT_AVATAR = require('./assets/python.png')
+export const HOME_URL = `${BASE_URL}/topics/timeline`
+export const TIMELINE_URL = `${HOME_URL}?show=all`
+export const LOGIN_URL = `${BASE_URL}/session`
+export const SELF_PROFILE = `${BASE_URL}/users/me`
+export const NOTIFICATION = `${SELF_PROFILE}/notification`
+
+export function getProfileUrl (username) {
+  return `${BASE_URL}/api/users/${username}`
+}
